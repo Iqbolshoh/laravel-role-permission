348a79d9fcdd68fa7b23a0b5c41ed16e18520ac2
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-17 00:10:38 +0500

    commit

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,10 +1,3 @@
-<x-filament-panels::page class="fi-dashboard-page">
-    @isset($this->filtersForm)
-        {{ $this->filtersForm }}
-    @endisset
-
-    <x-filament-widgets::widgets :columns="$this->getColumns()" :data="array_merge(
-        property_exists($this, 'filters') ? ['filters' => $this->filters] : [],
-        $this->getWidgetData()
-    )" :widgets="$this->getVisibleWidgets()" />
+<x-filament-panels::page>
+    <h1>Welcome to the Dashboard!</h1>
 </x-filament-panels::page>
