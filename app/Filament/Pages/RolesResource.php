e916c9bcab36107577cc168717d9c4d3631fc6b0
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-16 22:15:23 +0500

    commit

--- app/Filament/Pages/RolesResource.php
+++ app/Filament/Pages/RolesResource.php
@@ -3,10 +3,11 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use Filament\Support\Facades\FilamentIcon;
 
 class RolesResource extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-shield-check';
 
     protected static string $view = 'filament.pages.roles-resource';
 }
--- app/Filament/Pages/UsersResource.php
+++ app/Filament/Pages/UsersResource.php
@@ -6,7 +6,7 @@ use Filament\Pages\Page;
 
 class UsersResource extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     protected static string $view = 'filament.pages.users-resource';
 }
