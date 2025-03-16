b9a4e94828393c626357ec172ca93e32aad70c94
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-16 22:04:42 +0500

    commit

---
+++ app/Filament/Pages/RolesResource.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class RolesResource extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.roles-resource';
+}
