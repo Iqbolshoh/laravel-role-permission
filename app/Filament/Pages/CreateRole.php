82b27cf30ba73656f97578d37cffe41bf25be81c
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-17 13:46:36 +0500

    Add permission

---
+++ app/Filament/Pages/CreateRole.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\Auth;
+
+class CreateRole extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-plus-circle';
+    protected static string $view = 'filament.pages.create-role';
+    protected static ?string $navigationGroup = 'Roles';
+    protected static ?int $navigationSort = 1;
+
+    public static function canAccess(): bool
+    {
+        return Auth::user()?->can('role.create') ?? false;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/admin', function () {
+    return view('welcome');
+})->middleware('permission:test.create');
